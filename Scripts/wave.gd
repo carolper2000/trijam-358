e0faae3c3de3c70d9db4d193f4d5716af83b45f1
Carolina Inácio Pereira <carolina.inaciope@hes-so.ch>
2026-02-08 21:12:57 +0100

    feat: add ricochet

--- Scripts/wave.gd
+++ Scripts/wave.gd
@@ -1,6 +1,66 @@
+#extends Area2D
+#
+#@export var speed: float = 750.0
+#@onready var ray = $RayCast2D
+#
+#func _physics_process(delta: float) -> void:
+	#position += transform.x * speed * delta
+#
+#func _on_body_entered(_body: Node2D) -> void:
+	## On allonge temporairement le rayon pour être sûr de toucher le mur
+	## même si l'onde est déjà un peu à l'intérieur
+	#ray.target_position = Vector2(100, 0) 
+	#ray.force_raycast_update()
+	#
+	#if ray.is_colliding():
+		#var normal = ray.get_collision_normal()
+		#
+		## 1. Calcul du rebond exact
+		#var current_dir = transform.x
+		#var new_dir = current_dir.reflect(normal)
+		#
+		## 2. Application
+		#rotation = new_dir.angle()
+		#
+		## 3. Sortie forcée de la zone de collision pour éviter les bugs
+		## On replace l'onde au point de contact + un petit décalage
+		#global_position = ray.get_collision_point() + (normal * 5.0)
+	#else:
+		## Si ça traverse encore, c'est que le RayCast ne voit pas le Layer de l'obstacle
+		#print("Erreur : Le RayCast ne voit pas l'obstacle sur le Layer ", _body.collision_layer)
+
 extends Area2D
 
-var speed = 750
+@export var speed: float = 750.0
+@onready var ray = $RayCast2D
 
-func _physics_process(delta):
+func _physics_process(delta: float) -> void:
+	# On avance vers "l'avant" de l'objet
 	position += transform.x * speed * delta
+
+func _on_body_entered(_body: Node2D) -> void:
+	ray.force_raycast_update()
+	
+	if ray.is_colliding():
+		var normal = ray.get_collision_normal()
+		
+		# On récupère la direction actuelle en vecteur GLOBAL
+		var current_dir = transform.x
+		
+		# On calcule le rebond
+		# .bounce() est souvent plus robuste que .reflect() pour les normales physiques
+		var new_dir = current_dir.bounce(normal)
+		
+		# On applique la nouvelle direction
+		rotation = new_dir.angle()
+		
+		# ON SORT DU MUR : C'est ici que l'angle se "casse" si on ne le fait pas.
+		# On se place à l'impact + on pousse vers la normale.
+		global_position = ray.get_collision_point() + normal * 5.0
+		
+		print("Collision avec normale : ", normal, " Nouvel angle : ", rad_to_deg(rotation))
+	else:
+		# Si le RayCast ne voit rien, c'est un problème de Masque ou de Position
+		# On fait un rebond "par défaut" basé sur le mouvement pour ne pas traverser
+		rotation += PI
+		print("RayCast aveugle - Demi-tour de secours")
--- Scripts/wave_spawner.gd
+++ Scripts/wave_spawner.gd
@@ -1,6 +1,5 @@
 extends Node2D
 
-@onready var ray_cast_2d = $RayCast2D
 @export var Wave : PackedScene
 
 func _ready() -> void:
@@ -13,7 +12,7 @@ func _process(_delta: float) -> void:
 func spawn_waves() -> void:
 	# Instantiate wave
 	var wave: Node2D = Wave.instantiate()
-	# Give the ray cast global transform to the wave	
-	wave.transform = ray_cast_2d.global_transform
+	# Give this elements global transform to the wave
+	wave.transform = self.global_transform
 	# Add to main scene to avoid parent movement issues
 	get_tree().root.add_child(wave)
