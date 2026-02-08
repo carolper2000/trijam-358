f0d93e84968810eefa9ced2f714d938ee2989ad9
Carolina Inácio Pereira <carolina.inaciope@hes-so.ch>
2026-02-08 22:01:23 +0100

    feat: add more waves

--- Scripts/wave_spawner.gd
+++ Scripts/wave_spawner.gd
@@ -1,6 +1,8 @@
 extends Node2D
 
 @export var Wave : PackedScene
+@export var ExtraPairs: int = 2
+@export var Spread: int = 10
 
 func _ready() -> void:
 	pass
@@ -16,3 +18,17 @@ func spawn_waves() -> void:
 	wave.transform = self.global_transform
 	# Add to main scene to avoid parent movement issues
 	get_tree().root.add_child(wave)
+	
+	
+	for i in ExtraPairs + 1:
+		if i == 0: continue
+		var wave1: Node2D = Wave.instantiate()
+		var wave2: Node2D = Wave.instantiate()
+		wave1.transform = self.global_transform
+		wave2.transform = self.global_transform
+		
+		wave1.global_rotation -= deg_to_rad(i*Spread)
+		wave2.global_rotation += deg_to_rad(i*Spread)
+		
+		get_tree().root.add_child(wave1)
+		get_tree().root.add_child(wave2)
